569d63d4c9bfb55af85a24977b52ceb52f00b072
ilhanguvenerol <ilhanguvenerol@gmail.com>
2026-05-02 18:33:44 +0300

    Delete files to start over

--- Eternal Colosseum/Assets/Scripts/EnemyAI/IEnemyState.cs
+++
@@ -1,9 +0,0 @@
-namespace EternalColosseum.EnemyAI
-{
-    public interface IEnemyState
-    {
-        void Enter(EnemyController enemy);
-        void Execute(EnemyController enemy);
-        void Exit(EnemyController enemy);
-    }
-}
