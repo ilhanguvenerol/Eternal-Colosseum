7c2c4ae267b2628e184f93130e4aa93b5544f7e1
ilhanguvenerol <ilhanguvenerol@gmail.com>
2026-06-01 17:09:24 +0300

    sound instances

--- Eternal Colosseum/Assets/Scripts/GameLoop/MainMenuUI.cs
+++ Eternal Colosseum/Assets/Scripts/GameLoop/MainMenuUI.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    private void Start()
+    {
+        AudioManager.Instance.PlayMainMenuMusic();
+    }
     // Load the arena scene when the player clicks the "Start Game" button.
     public void StartGame()
     {
